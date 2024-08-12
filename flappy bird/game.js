53e865a8073441ae2bbcd9059a8f3075a54a305e
VencislavManoilov <vencislav2.manoilov@gmail.com>
2024-08-12 13:45:29 +0300

    Making flappy bird

---
+++ flappy bird/game.js
@@ -0,0 +1,26 @@
+// Suzdavame promenlivi
+let myX, myY, vragX = 750, vragY = 300;
+function init() {
+    // Kodut tuk se izpulnqva vednuj v nachaloto
+    myX = 100;
+    myY = 100;
+}
+function update() {
+    // Kodut tuk se izpulnqva (okolo) 100 puti v sekunda
+    myY = myY + 3;
+
+    vragX = vragX - 3;
+}
+function draw() {
+    // Tuk naprogramirai kakvo da se risuva
+    drawImage(backField, 0, 0, 800, 600);
+    drawImage(femaleAction, myX, myY, 60, 80);
+    drawImage(dogRun[3], vragX, vragY, 50, 50);
+}
+function mouseup() {
+    vragX = 750;
+}
+function keyup(key) {
+    // Pechatai koda na natisnatiq klavish
+    myY = myY - 100;
+}
